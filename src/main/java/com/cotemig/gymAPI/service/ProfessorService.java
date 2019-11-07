de7e4bd2fcc3522a02f8f76b0b3d21203fc0b308
gustavo <gustavopraxedes@live.com>
2019-11-06 21:16:01 -0300

    teste

--- src/main/java/com/cotemig/gymAPI/service/ProfessorService.java
+++ src/main/java/com/cotemig/gymAPI/service/ProfessorService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import com.cotemig.gymAPI.model.Professor;
 import com.cotemig.gymAPI.repository.ProfessorRepository;
 
-@Service("ProfessorService")
+@Service("professorService")
 public class ProfessorService implements IProfessorService{
 
 	@Autowired
